82346f339edb50b7d2eba3f8f29f8fbf6672bf67
SayiraQuirama <sayira.quirama@pragma.com.co>
2025-02-28 09:51:31 -0500

    Initial configuration

---
+++ src/main/java/com/example/food_court_ms_traceability/FoodCourtMsTraceabilityApplication.java
@@ -0,0 +1,13 @@
+package com.example.food_court_ms_traceability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCourtMsTraceabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodCourtMsTraceabilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/food_court_ms_traceability/FoodCourtMsTraceabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.food_court_ms_traceability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCourtMsTraceabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
